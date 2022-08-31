408562eb9278c0cb77996dc915fe204e74ebda4c
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-08-30 19:18:58 -0600

    procesoEjercicio1.php actualizada

--- procesoEjercicio1.php
+++ procesoEjercicio1.php
@@ -6,6 +6,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Proceso Ejercicio 1</title>
 </head>
+        <h1> &emsp;  Guía 1 - INTRODUCCION A PHP</h1>
+        &emsp;<strong>  Alumno: </strong> <p> &emsp;Gerardo José Villeda Erazo</p> <br>
+        &emsp;<strong>  Código: </strong> <p> &emsp;VE16I04001</p> <br>
+        &emsp;<strong>  Materia: </strong> <P> &emsp;Implantación de Sistemas - 2022</P>  <br>
 <body>
     <?php
         //Captura de datos del form
@@ -25,6 +29,11 @@
         echo "<br>";
     ?>
     <input type="button" onclick="history.back()" name="volver atrás" value="volver atrás">
+    <div class="container text-right">
+        <p class="d-inline">
+            <strong> Derechos reservados - Universidad de Sonsonate &copy; 2022</strong>
+        </p>
+    </div> 
 </body>
 </html>
 
