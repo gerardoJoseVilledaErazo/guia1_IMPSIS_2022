153ab5f70abe97b8c802a839aa2c5ad8b409fc4a
gerardoJoseVilledaErazo <gerardovilleda@gmail.com>
2022-08-30 20:14:37 -0600

    Ejercicio3.php creado

---
+++ Ejercicio3.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 3 - guia 1 | 2022</title>
+</head>
+        <h1> &emsp;  Guía 1 - INTRODUCCION A PHP</h1>
+        &emsp;<strong>  Alumno: </strong> <p> &emsp;Gerardo José Villeda Erazo</p> <br>
+        &emsp;<strong>  Código: </strong> <p> &emsp;VE16I04001</p> <br>
+        &emsp;<strong>  Materia: </strong> <P> &emsp;Implantación de Sistemas - 2022</P>  <br>
+<body>
+    
+</body>
+</html>
